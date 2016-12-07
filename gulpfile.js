298cf7151352f5bb9da02b463243f5ea72ce2bd8
Brent <bsuggs13@outlook.com>
2016-12-07 09:22:07 -0600

    reorganized gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,28 @@ var gulp = require('gulp'),
      image = require('gulp-image'),
 spriteSmith = require('gulp.spritesmith'); 
 
+gulp.task('compressImgs', function() {
+    return gulp.src(["./img/**/*.jpg"], { base: "./img"})
+    .pipe(image())
+    .pipe(gulp.dest("./dist/img"))
+});
 
-gulp.task('concatCSS', function() {
+gulp.task('spriteAvatars', ['compressImgs'], function() {
+    var spriteData = 
+        gulp.src('./dist/img/avatars/*.jpg') // source path of the sprite images
+            .pipe(spriteSmith({
+                imgPath: '../img/avatars/sprite.jpg',
+                imgName: 'sprite.jpg',
+                cssName: 'avatar.css',
+                padding: 2
+            }));
+
+    spriteData.img.pipe(gulp.dest('./dist/img/avatars')); // output path for the sprite
+   return spriteData.css.pipe(gulp.dest('./css')); // output path for the CSS
+});
+
+
+gulp.task('concatCSS', ['spriteAvatars'], function() {
    return gulp.src([
        'css/normalize.css',
        'css/foundation.css',
@@ -34,7 +54,7 @@ gulp.task('minifyCSS', ['concatCSS'], function() {
      .pipe(gulp.dest("css"));
 });
 
-gulp.task('concatScripts', function() {
+gulp.task('concatScripts',  function() {
    return gulp.src([
         'js/jquery.js',
         'js/fastclick.js',
@@ -54,25 +74,6 @@ gulp.task('minifyScripts', ['concatScripts'], function() {
     .pipe(gulp.dest("js"));
 });
 
-gulp.task('compressImgs', function() {
-    return gulp.src(["./img/**/*.jpg", "./img/**/*.png" ], { base: "./img"})
-    .pipe(image())
-    .pipe(gulp.dest("./dist/img"))
-});
-
-//gulp.task('spriteAvatars', ['compressImgs'], function() {
-//    var spriteData = 
-//        gulp.src('./dist/img/avatars/*.jpg') // source path of the sprite images
-//            .pipe(spriteSmith({
-//                imgPath: '../img/avatars/sprite.jpg',
-//                imgName: 'sprite.jpg',
-//                cssName: 'avatar.css',
-//            }));
-//
-//    spriteData.img.pipe(gulp.dest('./dist/img/avatars')); // output path for the sprite
-//    spriteData.css.pipe(gulp.dest('./css')); // output path for the CSS
-//});
-
 
 gulp.task('tidyHTML', function() {
    gulp.src('index.html') 
@@ -84,7 +85,7 @@ gulp.task('clean', function() {
     del(['dist', 'js/scripts*.js*', 'imgs/**']);
 })
 
-gulp.task('build',['tidyHTML','minifyCSS', 'minifyScripts', 'compressImgs'], function() {
+gulp.task('build',['tidyHTML', 'minifyCSS', 'minifyScripts'], function() {
     gulp.src(["css/styles.css", "css/styles.min.css", "js/scripts.js",
             "js/scripts.min.js"], { base: './'} )
         .pipe(gulp.dest('dist'));
